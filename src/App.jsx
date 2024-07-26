0f32af2f1e3ea45555cc8a0c40be8eb85c207374
Liburna Berisha <115898216+Liburna1@users.noreply.github.com>
2024-07-26 15:32:41 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
 
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import AboutUs from "./pages/AboutUs";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="AboutUs" element={<AboutUs />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
